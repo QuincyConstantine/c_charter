d037f4e44dd26645eb595c5071b8e8e794f450e0
QuincyConstantine <hlogrono4@gmail.com>
2024-07-09 02:20:08 +0100

    "slider indicator added"

--- lib/slider_screen.dart
+++ lib/slider_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class SliderScreen extends StatefulWidget {
   @override
@@ -36,16 +37,31 @@ class _SliderScreenState extends State<SliderScreen> {
             bottom: 30,
             left: 20,
             right: 20,
-            child: _currentPage == _images.length - 1
-                ? ElevatedButton(
-              onPressed: () async {
-                SharedPreferences prefs = await SharedPreferences.getInstance();
-                await prefs.setBool('seen', true);
-                Navigator.pushReplacementNamed(context, '/home');
-              },
-              child: Text('Enter'),
-            )
-                : SizedBox.shrink(),
+            child: Column(
+              children: [
+                SmoothPageIndicator(
+                  controller: _pageController,
+                  count: _images.length,
+                  effect: ScrollingDotsEffect(
+                    activeDotColor: Colors.blue,
+                    dotColor: Colors.grey,
+                    dotHeight: 10,
+                    dotWidth: 10,
+                  ),
+                ),
+                SizedBox(height: 16),
+                _currentPage == _images.length - 1
+                    ? ElevatedButton(
+                  onPressed: () async {
+                    SharedPreferences prefs = await SharedPreferences.getInstance();
+                    await prefs.setBool('seen', true);
+                    Navigator.pushReplacementNamed(context, '/home');
+                  },
+                  child: Text('Enter'),
+                )
+                    : SizedBox.shrink(),
+              ],
+            ),
           ),
         ],
       ),
